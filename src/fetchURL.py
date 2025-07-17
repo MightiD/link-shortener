4ad5d24d4244e6b9daba160588e24514792dddac
MightiD <128378210+MightiD@users.noreply.github.com>
2025-07-17 11:13:02 +0100

    stop printing the links when trying to get them from the json file and change json path

--- src/fetchURL.py
+++ src/fetchURL.py
@@ -1,14 +1,12 @@
 import json
 
-jsonPath = "../../data/links.json"
+jsonPath = "../data/links.json"
 
 def fetchUrl(shortened):
     with open(jsonPath, 'r') as file:
         json_data = json.load(file)
-        print(json_data)
     try:
         link = json_data[shortened]
-        print(link)
         return link
     except:
         return "link not found"
