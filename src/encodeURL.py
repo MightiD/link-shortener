731f76c15712b3992e0789beff46feb82ef39295
MightiD <128378210+MightiD@users.noreply.github.com>
2025-07-17 12:17:39 +0100

    cleaned up code, apparently it stopped duplicate entries by itself automatically this way

--- src/encodeURL.py
+++ src/encodeURL.py
@@ -4,14 +4,16 @@ import json
 jsonPath = "../data/links.json"
 
 def storeUrl(url, shortened):
-    with open(jsonPath, 'r') as file:
-        json_data = json.load(file)
-    if json_data[shortened]: #if shortened sha is already present, exit the function
-        return
-    json_data[shortened] = url
-    with open(jsonPath, 'w') as file:
-        json.dump(json_data, file, indent=4)
+    # read file
+    with open(jsonPath) as file:
+        jsonData = json.load(file)
     
+    # update file
+    jsonData.update({shortened: url})
+
+    # write to file
+    with open(jsonPath, "w") as file:
+        json.dump(jsonData, file, indent=4)
 
 def encodeUrl(url):
     sha = hashlib.sha1(url.encode()).hexdigest()
