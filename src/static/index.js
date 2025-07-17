47f87a4af7c03e6afb414fd655cf1949a3f846d1
MightiD <128378210+MightiD@users.noreply.github.com>
2025-07-17 15:21:55 +0100

    forgot to stage in last commit

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Hello world")
+    //get the form, listen for when user submits
+    document.getElementById("UrlForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const urlInput = document.getElementById("Url").value.trim();
+
+        if (urlInput != "") {
+            fetch(`http://127.0.0.1:8080/api/v1/encode?url=${urlInput}`)
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                display = document.getElementById("shortenedUrl").innerText = data
+            })
+        }
+    })
 });
