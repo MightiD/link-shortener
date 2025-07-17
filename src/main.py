963b9e6f5e6ea9ad4c9f26fc81523325e88bfa38
MightiD <128378210+MightiD@users.noreply.github.com>
2025-07-17 11:31:49 +0100

    returns html page

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 from encodeURL import encodeUrl
 from fetchURL import fetchUrl
 
@@ -15,10 +15,14 @@ def encode(url):
     return(f"{SERVER_URL}/{encodeUrl(url)}")
 
 @app.route("/<string:url>")
-def fetch(url):
+def fetch(url):    
     fetched = fetchUrl(url)
+
     if fetched == "link not found":
-        return "link not found"
+        return fetched
     else:
         return redirect(f"http://{fetchUrl(url)}")
 
+@app.route("/")
+def rootPage():
+    return render_template("index.html")
